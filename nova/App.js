6ef724944b13050341c9f0eea10fb30e399a73e4
Miguel Sevilla <sev.miguelssi@gmail.com>
2023-03-30 16:35:42 -0400

    Test commit

--- nova/App.js
+++ nova/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Can you see this? </Text>
+      <Text> Git commit and push test - Miguel</Text>
 
       <StatusBar style="auto" />
     </View>
